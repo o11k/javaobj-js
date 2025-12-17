950e3bc7c9b2ba5c40daa77216918d78cc5099a4
Ori Levenglick <61731312+o11k@users.noreply.github.com>
2025-12-17 00:41:19 -0600

    Test: factor utility methods to utility classes

--- tests/generatetests/src/main/java/com/o11k/GenerateTests.java
+++ tests/generatetests/src/main/java/com/o11k/GenerateTests.java
@@ -5,89 +5,11 @@ import java.lang.reflect.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class GenerateTests {
+class Randomizer {
     static final float CHANCE_NAN = 0.05f;
     static final float CHANCE_INFINITY = 0.05f;
     static final float CHANCE_SUBNORMAL = 0.05f;
     static final float CHANCE_ZERO = 0.05f;
-
-    static final float CHANCE_PRIMITIVE = 0.50f;
-
-    static final int NUM_ITEMS_TO_WRITE = 10_000;
-
-    static final String PATH_DIR = "tests/tmp";
-    static final String PATH_TXT = PATH_DIR + "/expected.txt";
-    static final String PATH_SER = PATH_DIR + "/serialized.ser";
-
-    static final String[] CHAR_PRINT_LOOKUP = makeCharPrintLookup();
-
-    static String[] makeCharPrintLookup() {
-        final String[] result = new String[Character.MAX_VALUE+1];
-
-        for (int i=0; i<=Character.MAX_VALUE; i++) {
-            char c = (char)i;
-            String s = null;
-
-            // Special escapes
-            switch (c) {
-                case '"': s = "\\\""; break;
-                case '\'': s = "\\'"; break;
-                case '\b': s = "\\b"; break;
-                case '\f': s = "\\f"; break;
-                case '\n': s = "\\n"; break;
-                case '\r': s = "\\r"; break;
-                case '\t': s = "\\t"; break;
-                case '\\': s = "\\\\"; break;
-
-                // ASCII space is printable
-                case ' ': s = " "; break;
-            }
-
-            // Printable
-            // Unicode general category in class L, M, N, P, or S
-            if (s == null) {
-                switch (Character.getType(c)) {
-                    case Character.LOWERCASE_LETTER:
-                    case Character.MODIFIER_LETTER:
-                    case Character.OTHER_LETTER:
-                    case Character.TITLECASE_LETTER:
-                    case Character.UPPERCASE_LETTER:
-
-                    case Character.COMBINING_SPACING_MARK:
-                    case Character.ENCLOSING_MARK:
-                    case Character.NON_SPACING_MARK:
-
-                    case Character.DECIMAL_DIGIT_NUMBER:
-                    case Character.LETTER_NUMBER:
-                    case Character.OTHER_NUMBER:
-
-                    case Character.CONNECTOR_PUNCTUATION:
-                    case Character.DASH_PUNCTUATION:
-                    case Character.END_PUNCTUATION:
-                    case Character.FINAL_QUOTE_PUNCTUATION:
-                    case Character.INITIAL_QUOTE_PUNCTUATION:
-                    case Character.OTHER_PUNCTUATION:
-                    case Character.START_PUNCTUATION:
-
-                    case Character.CURRENCY_SYMBOL:
-                    case Character.MODIFIER_SYMBOL:
-                    case Character.MATH_SYMBOL:
-                    case Character.OTHER_SYMBOL:
-                        s = Character.toString(c);
-                        break;
-                }
-            }
-
-            // Unicode escape
-            if (s == null) {
-                s = String.format("\\u%04x", (int)c);
-            }
-
-            result[c] = s;
-        }
-
-        return result;
-    }
 
     static byte nextByte(Random rnd) { byte[] bs = {0}; rnd.nextBytes(bs); return bs[0]; }
     static char nextChar(Random rnd) { return (char)rnd.nextInt(1 << 16); }
@@ -131,6 +53,99 @@ public class GenerateTests {
     static long nextLong(Random rnd) { return rnd.nextLong(); }
     static short nextShort(Random rnd) { return (short)rnd.nextInt(1 << 16); }
     static boolean nextBoolean(Random rnd) { return rnd.nextBoolean(); }
+}
+
+class ToJS {
+    static boolean isPrintable(char c) {
+        switch (Character.getType(c)) {
+            case Character.LOWERCASE_LETTER:
+            case Character.MODIFIER_LETTER:
+            case Character.OTHER_LETTER:
+            case Character.TITLECASE_LETTER:
+            case Character.UPPERCASE_LETTER:
+
+            case Character.COMBINING_SPACING_MARK:
+            case Character.ENCLOSING_MARK:
+            case Character.NON_SPACING_MARK:
+
+            case Character.DECIMAL_DIGIT_NUMBER:
+            case Character.LETTER_NUMBER:
+            case Character.OTHER_NUMBER:
+
+            case Character.CONNECTOR_PUNCTUATION:
+            case Character.DASH_PUNCTUATION:
+            case Character.END_PUNCTUATION:
+            case Character.FINAL_QUOTE_PUNCTUATION:
+            case Character.INITIAL_QUOTE_PUNCTUATION:
+            case Character.OTHER_PUNCTUATION:
+            case Character.START_PUNCTUATION:
+
+            case Character.CURRENCY_SYMBOL:
+            case Character.MODIFIER_SYMBOL:
+            case Character.MATH_SYMBOL:
+            case Character.OTHER_SYMBOL:
+                return true;
+
+            default:
+                return false;
+        }
+    }
+
+    static String charToJS(char c) {
+        switch (c) {
+            case '"':  return "\\\"";
+            case '\'': return "\\'";
+            case '\b': return "\\b";
+            case '\f': return "\\f";
+            case '\n': return "\\n";
+            case '\r': return "\\r";
+            case '\t': return "\\t";
+            case '\\': return "\\\\";
+        }
+
+        if (isPrintable(c))
+            return Character.toString(c);
+
+        // JSON Unicode escape
+        return String.format("\\u%04x", (int)c);
+    }
+
+    static String toJs(Object obj) throws Exception {
+        if (obj instanceof Byte)      return obj.toString();
+        if (obj instanceof Character) return "'" + charToJS((Character)obj) + "'";
+        if (obj instanceof Double)    return obj.toString();
+        if (obj instanceof Float)     return Double.toString(((Float)obj).doubleValue());
+        if (obj instanceof Integer)   return obj.toString();
+        if (obj instanceof Long)      return obj.toString() + "n";
+        if (obj instanceof Short)     return obj.toString();
+        if (obj instanceof Boolean)   return obj.toString();
+
+        if (obj instanceof String) return '"' + ((String)obj).chars().mapToObj(c -> charToJS((char)c)).collect(Collectors.joining()) + '"';
+
+        String result = "";
+
+        result += "{";
+        Field[] fields = obj.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            result += toJs(field.getName()) + ":" + toJs(field.get(obj)) + ",";
+        }
+        result += "}";
+
+        return result;
+
+    }
+}
+
+
+public class GenerateTests {
+    static final float CHANCE_PRIMITIVE = 0.50f;
+
+    static final int NUM_ITEMS_TO_WRITE = 10_000;
+
+    static final String PATH_DIR = "tests/tmp";
+    static final String PATH_TXT = PATH_DIR + "/expected.txt";
+    static final String PATH_SER = PATH_DIR + "/serialized.ser";
+
 
     static void writePrimitive(Random rnd, FileWriter outExpected, ObjectOutputStream oos) throws Exception {
         final char[] typecodes = {'B', 'C', 'D', 'F', 'I', 'J', 'S', 'Z'};
@@ -138,44 +153,44 @@ public class GenerateTests {
 
         switch (typecode) {
             case 'B':
-                final byte b = nextByte(rnd);
+                final byte b = Randomizer.nextByte(rnd);
                 oos.writeByte(b);
-                outExpected.write(typecode + toJs(b) + '\n');
+                outExpected.write(typecode + ToJS.toJs(b) + '\n');
                 break;
             case 'C':
-                final char c = nextChar(rnd);
+                final char c = Randomizer.nextChar(rnd);
                 oos.writeChar(c);
-                outExpected.write(typecode + toJs(c) + '\n');
+                outExpected.write(typecode + ToJS.toJs(c) + '\n');
                 break;
             case 'D':
-                final double d = nextDouble(rnd);
+                final double d = Randomizer.nextDouble(rnd);
                 oos.writeDouble(d);
-                outExpected.write(typecode + toJs(d) + '\n');
+                outExpected.write(typecode + ToJS.toJs(d) + '\n');
                 break;
             case 'F':
-                final float f = nextFloat(rnd);
+                final float f = Randomizer.nextFloat(rnd);
                 oos.writeFloat(f);
-                outExpected.write(typecode + toJs(f) + '\n');
+                outExpected.write(typecode + ToJS.toJs(f) + '\n');
                 break;
             case 'I':
-                final int i_ = nextInt(rnd);
+                final int i_ = Randomizer.nextInt(rnd);
                 oos.writeInt(i_);
-                outExpected.write(typecode + toJs(i_) + '\n');
+                outExpected.write(typecode + ToJS.toJs(i_) + '\n');
                 break;
             case 'J':
-                final long l = nextLong(rnd);
+                final long l = Randomizer.nextLong(rnd);
                 oos.writeLong(l);
-                outExpected.write(typecode + toJs(l) + '\n');
+                outExpected.write(typecode + ToJS.toJs(l) + '\n');
                 break;
             case 'S':
-                final short s = nextShort(rnd);
+                final short s = Randomizer.nextShort(rnd);
                 oos.writeShort(s);
-                outExpected.write(typecode + toJs(s) + '\n');
+                outExpected.write(typecode + ToJS.toJs(s) + '\n');
                 break;
             case 'Z':
-                final boolean z = nextBoolean(rnd);
+                final boolean z = Randomizer.nextBoolean(rnd);
                 oos.writeBoolean(z);
-                outExpected.write(typecode + toJs(z) + '\n');
+                outExpected.write(typecode + ToJS.toJs(z) + '\n');
                 break;
             default:
                 throw new Exception("Unexpected typecode: " + typecode);
@@ -193,43 +208,18 @@ public class GenerateTests {
         boolean z;
     }
 
-    static String toJs(Object obj) throws Exception {
-        if (obj instanceof Byte) return obj.toString();
-        if (obj instanceof Character) return "'" + CHAR_PRINT_LOOKUP[(char)obj] + "'";
-        if (obj instanceof Double) return obj.toString();
-        if (obj instanceof Float) return Double.toString(((Float)obj).doubleValue());
-        if (obj instanceof Integer) return obj.toString();
-        if (obj instanceof Long) return obj.toString() + "n";
-        if (obj instanceof Short) return obj.toString();
-        if (obj instanceof Boolean) return obj.toString();
-
-        if (obj instanceof String) return '"' + ((String)obj).chars().mapToObj(c -> CHAR_PRINT_LOOKUP[c]).collect(Collectors.joining()) + '"';
-
-        String result = "";
-
-        result += "{";
-        Field[] fields = obj.getClass().getDeclaredFields();
-        for (Field field : fields) {
-            result += toJs(field.getName()) + ":" + toJs(field.get(obj)) + ",";
-        }
-        result += "}";
-
-        return result;
-
-    }
-
     static void writeObject(Random rnd, FileWriter outExpected, ObjectOutputStream oos) throws Exception {
         A obj = new A();
-        obj.b = nextByte(rnd);
-        obj.c = nextChar(rnd);
-        obj.d = nextDouble(rnd);
-        obj.f = nextFloat(rnd);
-        obj.i = nextInt(rnd);
-        obj.j = nextLong(rnd);
-        obj.s = nextShort(rnd);
-        obj.z = nextBoolean(rnd);
+        obj.b = Randomizer.nextByte(rnd);
+        obj.c = Randomizer.nextChar(rnd);
+        obj.d = Randomizer.nextDouble(rnd);
+        obj.f = Randomizer.nextFloat(rnd);
+        obj.i = Randomizer.nextInt(rnd);
+        obj.j = Randomizer.nextLong(rnd);
+        obj.s = Randomizer.nextShort(rnd);
+        obj.z = Randomizer.nextBoolean(rnd);
 
-        outExpected.write("L" + toJs(obj) + "\n");
+        outExpected.write("L" + ToJS.toJs(obj) + "\n");
         oos.writeObject(obj);
     }
 
