ee74b2d40998c8d1e73c00c74f02b0fe4193d8f4
Ori Levenglick <61731312+o11k@users.noreply.github.com>
2025-12-23 22:12:18 -0600

    Define AST types

---
+++ src/ast.ts
@@ -0,0 +1,265 @@
+import * as c from './constants';
+
+
+// ========== CONTRACT ==========
+//
+// - If a node has children, their spans add up exactly to the parent's span, and do not overlap
+// - Handles are consecutive, epoch increments on "reset" or "exception"
+// - Types match
+//     - ClassDescNode  > PrevObjectNode is a ClassDesc
+//     - NewEnumNode    > ObjectNode     is a String
+//     - ObjectDescNode > ObjectNode     is a String
+//     - ExceptionNode  > ObjectNode     is Throwable
+// - "values" match their respective "fields" / array classData
+
+
+export interface Ast {
+    data: Uint8Array
+    root: RootNode
+    error?: string
+}
+export default Ast
+
+export interface RootNode extends BaseNode<[MagicNode, VersionNode, ContentsNode]> {
+    type: "root"
+}
+
+export interface MagicNode extends BaseNode<null> {type: "magic", value: typeof c.STREAM_MAGIC}
+export interface VersionNode extends BaseNode<null> {type: "version", value: typeof c.STREAM_VERSION}
+
+export interface ContentsNode extends BaseNode<(BlockDataSequenceNode | ObjectNode)[]> {
+    type: "contents"
+}
+
+export interface BaseNode<C extends BaseNode<any>[] | null> {
+    type: string
+    span: {start: number, end: number}
+    children: C
+}
+
+
+// ========== Primitives ==========
+
+export type PrimitiveNode = NumberNode | CharNode | BooleanNode | LongNode
+
+export interface BasePrimitiveNode extends BaseNode<null> {
+    type: "primitive"
+    dataType: string
+    value: number | string | boolean | bigint
+}
+export interface NumberNode extends BasePrimitiveNode {
+    dataType: "byte" | "unsigned-byte" | "short" | "unsigned-short" | "int" | "float" | "double"
+    value: number
+}
+export type ByteNode          = NumberNode & {dataType: "byte"}
+export type UnsignedByteNode  = NumberNode & {dataType: "unsigned-byte"}
+export type ShortNode         = NumberNode & {dataType: "short"}
+export type UnsignedShortNode = NumberNode & {dataType: "unsigned-short"}
+export type IntNode           = NumberNode & {dataType: "int"}
+export type FloatNode         = NumberNode & {dataType: "float"}
+export type DoubleNode        = NumberNode & {dataType: "double"}
+
+export interface CharNode extends BasePrimitiveNode {
+    dataType: "char"
+    value: string
+}
+export interface BooleanNode extends BasePrimitiveNode {
+    dataType: "boolean"
+    value: boolean
+}
+export interface LongNode extends BasePrimitiveNode {
+    dataType: "long"
+    value: bigint
+}
+
+export interface UtfNode extends BaseNode<[UnsignedShortNode, UtfBodyNode]> {
+    type: "utf"
+}
+export interface LongUtfNode extends BaseNode<[LongNode, UtfBodyNode]> {
+    type: "long-utf"
+}
+export interface UtfBodyNode extends BaseNode<null> {
+    type: "utf-body"
+    value: string
+}
+
+export interface BytesNode extends BaseNode<null> {
+    type: "bytes"
+}
+
+export interface SkippedNode extends BaseNode<null> {
+    type: "skipped"
+}
+
+
+// ========== Block Data ==========
+
+export interface BlockDataSequenceNode extends BaseNode<BlockDataNode[]> {
+    type: "blockdata-sequence"
+    values: (PrimitiveNode | UtfNode | SkippedNode)[]
+}
+
+export interface BlockDataNode extends BaseNode<
+[TC_BLOCKDATA_Node,     UnsignedByteNode, BytesNode] |
+[TC_BLOCKDATALONG_Node, LongNode,         BytesNode]
+> {
+    type: "blockdata"
+}
+
+
+// ========== Objects ==========
+
+export type ObjectNode = NewObjectNode | NewClassNode | NewArrayNode | NewStringNode | NewEnumNode | NewClassDescNode | PrevObjectNode | NullNode | ExceptionNode | ResetNode
+export type ClassDescNode = NewClassDescNode | NullNode | PrevObjectNode
+
+export interface BaseObjectNode<C extends [TCNode, ...BaseNode<any>[]]> extends BaseNode<C> {
+    type: "object"
+    objectType: string
+}
+
+export interface NewObjectNode extends BaseObjectNode<
+    [TC_OBJECT_Node, ClassDescNode, ...ClassDataNode[]]
+> {
+    objectType: "new-object"
+    handle: Handle
+}
+export interface NewClassNode extends BaseObjectNode<[TC_CLASS_Node, ClassDescNode]> {
+    objectType: "new-class"
+    handle: Handle
+}
+export interface NewArrayNode extends BaseObjectNode<[TC_ARRAY_Node, ClassDescNode, IntNode, ValuesNode]> {
+    objectType: "new-array"
+    handle: Handle
+}
+export interface NewStringNode extends BaseObjectNode<
+    [TC_STRING_Node,     UtfNode] |
+    [TC_LONGSTRING_Node, LongUtfNode]
+> {
+    objectType: "new-string"
+    handle: Handle
+}
+export interface NewEnumNode extends BaseObjectNode<[TC_ENUM_Node, ClassDescNode, ObjectNode]> {
+    objectType: "new-enum"
+    handle: Handle
+}
+export interface NewClassDescNode extends BaseObjectNode<
+    [TC_CLASSDESC_Node, UtfNode, LongNode, ClassDescInfoNode] |
+    [TC_PROXYCLASSDESC_Node, ProxyClassDescInfoNode]
+> {
+    objectType: "new-class-desc"
+    handle: Handle
+}
+export interface PrevObjectNode extends BaseObjectNode<[TC_REFERENCE_Node, IntNode]> {
+    objectType: "prev-object"
+    value: Handle
+}
+export interface NullNode extends BaseObjectNode<[TC_NULL_Node]> {
+    objectType: "null"
+}
+export interface ExceptionNode extends BaseObjectNode<[TC_EXCEPTION_Node, ObjectNode]> {
+    objectType: "exception"
+    exceptionEpoch: number
+    newEpoch: number
+}
+export interface ResetNode extends BaseObjectNode<[TC_RESET_Node]> {
+    objectType: "reset"
+    newEpoch: number
+}
+
+export interface ClassDescInfoNode extends BaseNode<[ByteNode, FieldsNode, ContentsNode, TC_ENDBLOCKDATA_Node, ClassDescNode]> {
+    type: "class-desc-info"
+}
+export interface ProxyClassDescInfoNode extends BaseNode<[IntNode, ...UtfNode[], ContentsNode, TC_ENDBLOCKDATA_Node, ClassDescNode]> {
+    type: "proxy-class-desc-info"
+}
+
+
+// ========== Fields & Values ==========
+
+export type ClassDataNode = NoWrClassNode | WrClassNode | ExternalClassNode | OldExternalClassNode
+
+export interface BaseClassDataNode<C extends BaseNode<any>[] | null> extends BaseNode<C> {
+    type: "class-data"
+    classType: string
+}
+export interface NoWrClassNode extends BaseClassDataNode<[ValuesNode]> {
+    classType: "serializable"
+    writeMethod: false
+}
+export interface WrClassNode extends BaseClassDataNode<
+    [ValuesNode, ContentsNode] |
+
+    // A write method can choose to violate the spec and write its values late, or not at all.
+    // Why? Because fuck you that's why.
+    [ContentsNode, ValuesNode, ContentsNode] |
+    [ContentsNode, ValuesNode] |
+    [ContentsNode]
+> {
+    classType: "serializable"
+    writeMethod: true
+}
+export interface ExternalClassNode extends BaseClassDataNode<[ContentsNode]> {
+    classType: "externalizable"
+    protocolVersion: 2
+}
+export interface OldExternalClassNode extends BaseClassDataNode<(ObjectNode | PrimitiveNode | UtfNode | BytesNode)[]> {
+    classType: "externalizable"
+    protocolVersion: 1
+}
+
+export interface FieldsNode extends BaseNode<[ShortNode, ...FieldDescNode[]]> {
+    type: "fields"
+}
+export interface ValuesNode extends BaseNode<(PrimitiveNode | ObjectNode)[]> {
+    type: "values"
+}
+
+export type FieldDescNode = PrimitiveDescNode | ObjectDescNode
+export interface PrimitiveDescNode extends BaseNode<[UnsignedByteNode, UtfNode]> {
+    type: "field-desc"
+    fieldType: "primitive"
+}
+export interface ObjectDescNode extends BaseNode<[UnsignedByteNode, UtfNode, ObjectNode]> {
+    type: "field-desc"
+    fieldType: "object"
+}
+
+
+// ========== Misc. ==========
+
+export interface TCNode extends BaseNode<null> {
+    type: "tc"
+    value: typeof c.TC_NULL |
+           typeof c.TC_REFERENCE |
+           typeof c.TC_CLASSDESC |
+           typeof c.TC_OBJECT |
+           typeof c.TC_STRING |
+           typeof c.TC_ARRAY |
+           typeof c.TC_CLASS |
+           typeof c.TC_BLOCKDATA |
+           typeof c.TC_ENDBLOCKDATA |
+           typeof c.TC_RESET |
+           typeof c.TC_BLOCKDATALONG |
+           typeof c.TC_EXCEPTION |
+           typeof c.TC_LONGSTRING |
+           typeof c.TC_PROXYCLASSDESC |
+           typeof c.TC_ENUM
+}
+
+export type TC_NULL_Node           = TCNode & {value: typeof c.TC_NULL}
+export type TC_REFERENCE_Node      = TCNode & {value: typeof c.TC_REFERENCE}
+export type TC_CLASSDESC_Node      = TCNode & {value: typeof c.TC_CLASSDESC}
+export type TC_OBJECT_Node         = TCNode & {value: typeof c.TC_OBJECT}
+export type TC_STRING_Node         = TCNode & {value: typeof c.TC_STRING}
+export type TC_ARRAY_Node          = TCNode & {value: typeof c.TC_ARRAY}
+export type TC_CLASS_Node          = TCNode & {value: typeof c.TC_CLASS}
+export type TC_BLOCKDATA_Node      = TCNode & {value: typeof c.TC_BLOCKDATA}
+export type TC_ENDBLOCKDATA_Node   = TCNode & {value: typeof c.TC_ENDBLOCKDATA}
+export type TC_RESET_Node          = TCNode & {value: typeof c.TC_RESET}
+export type TC_BLOCKDATALONG_Node  = TCNode & {value: typeof c.TC_BLOCKDATALONG}
+export type TC_EXCEPTION_Node      = TCNode & {value: typeof c.TC_EXCEPTION}
+export type TC_LONGSTRING_Node     = TCNode & {value: typeof c.TC_LONGSTRING}
+export type TC_PROXYCLASSDESC_Node = TCNode & {value: typeof c.TC_PROXYCLASSDESC}
+export type TC_ENUM_Node           = TCNode & {value: typeof c.TC_ENUM}
+
+export type Handle = {epoch: number, handle: number}
